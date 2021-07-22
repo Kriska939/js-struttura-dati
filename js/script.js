2ae038531c3f1377c3a73d7f36aa270538546664
Kriska939 <cristinacorrado93@gmail.com>
2021-07-22 15:55:23 +0200

    added basic structure + comments

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* DESCRIZIONE:
+
+Proviamo a ripetere quanto visto in classe, creando la struttura che riteniamo più adeguata per rappresentare una carta di Magic.
+Una volta definita la struttura, stampiamo sulla pagina HTML tutte le informazioni relative alla carta stessa, senza particolare attenzione a dettagli grafici (va bene una lista coi tag UL e LI)
+
+*/
+
+/* --------------STEPS----------------
+
+1. Per rappresentare la carta mi serve un oggetto (creo)
+2. Nell'oggetto mi serve mappare tutte le proprietà (struttura template)
+
+-------------------------------!
+N.B: mappatura da fare tenendo conto di necessità di filtri !
+-------------------------------!
+
+3. Stampo informazioni su HTML (brackets?)
+*/
+
